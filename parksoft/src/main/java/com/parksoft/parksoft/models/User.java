d05cfc4968ac41e5115abe8233478e47abee897c
Angie20000 <Valflorezvar@gmail.com>
2025-03-20 14:23:37 -0500

    trabajo de diseño

---
+++ parksoft/src/main/java/com/parksoft/parksoft/models/User.java
@@ -0,0 +1,63 @@
+package com.parksoft.parksoft.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name= "user")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true,length = 200, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
+
+}
---
+++ parksoft/src/test/java/com/parksoft/parksoft/ParksoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parksoft.parksoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
